1a7626be6fa88107c58f0c7fe3c9112434a33c22
huangziliang <289043018@qq.com>
2015-08-07 10:22:35 +0800

    add code

--- src/main/java/com/hand/scannerfile/App.java
+++ src/main/java/com/hand/scannerfile/App.java
@@ -10,14 +10,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        printfile(new File("../scannerfile"),1);
     }
-    public void printfile(File F){
+    public static void printfile(File F,int tab){
     	if(F.isDirectory()){
     		File next[] = F.listFiles();
     		for (int i = 0; i < next.length; i++) {
-				if(next[i].isFile()){
-					System.out.println(next[i].getName());
+    			for (int j = 0; j < tab; j++) {
+					System.out.print("|--");
+				}
+    			System.out.println(next[i].getName());
+				if(next[i].isDirectory()){
+					printfile(next[i],tab++);
 				}
 			}
     	}
